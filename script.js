8cc83fa9b192a18d79287c0944f63388607fd982
Elyabe <elyabebat@gmail.com>
2026-03-12 16:48:18 -0300

    get function

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const userName = document.getElementById('name');
 const userAge = document.getElementById('age');
 const userEmail = document.getElementById('email');
+const table = document.getElementById('table')
 
-//
+// Mostrar infos na table
+async function getUsers() {
+    const response = await fetch("http://localhost:8000/api/users");
+    const data = await response.json();
+    const users = data.users;
 
+    table.innerHTML = "";
 
+    users.forEach(user => {
+        table.innerHTML += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.age}</td>
+                <td>${user.email}</td>
+            </tr>
+        `;
+    });
+    
+}
+
+getUsers();
 
 // Função criar usuário
 async function createUser() {
